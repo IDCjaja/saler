20e6ea48e51346ac1e920901fd12091e27200a82
xanadu-zxl <xanadu.zxl@gamil.com>
2020-08-03 16:27:14 +0800

    feat：排行榜新建客户，预约客户，来电客户

--- src/components/admin/ranking_list.vue
+++ src/components/admin/ranking_list.vue
@@ -25,7 +25,7 @@
       </p>
       <p :key="item.id" class="ranking_list_content_body" v-for="item in list" v-show="showData">
         <span>{{ item.saler }}</span>
-        <span>{{ item.count }}</span>
+        <span>{{ item.customer_count }}</span>
         <span>{{ item.top }}</span>
       </p>
     </div>
@@ -39,18 +39,19 @@ export default {
   data() {
     return {
       type: "新建客户",
-      customer_type: "arrive_visitor",
+      source: "到访客户",
       date: "2020/5/15 - 2021/5/15",
       show: false,
-      startDate: "2020/5/15",
-      endDate: "2021/5/15",
+      startDate: "2020-05-15",
+      endDate: "2021-05-15",
       minDate: new Date(2010, 0, 1),
       maxDate: new Date(2100, 0, 31),
       types: [
-        { text: "预约客户", value: "预约客户" },
         { text: "新建客户", value: "新建客户" },
+        { text: "来电客户", value: "来电客户" },
         { text: "认购客户", value: "认购客户" },
         { text: "签约客户", value: "签约客户" },
+        { text: "预约客户", value: "预约客户" },
       ],
       list: [],
       showData: true,
@@ -61,46 +62,96 @@ export default {
   },
   mounted() {
     // 数据初始化
-    let params = {
-      customer_type: "arrive_visitor",
-      start_date: this.startDate,
-      end_date: this.endDate,
-    };
-    api.getAdminSalerTopAPI(params).then((res) => {
-      let list = res.data.query_top;
+    let sql = `select saler, count(customer_name) as customer_count from fdc_form_1_1 where source ='${this.source}' and to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+    api.getSqlJsonAPI(sql).then((res) => {
+      let list = res.data;
       this.list = total.rank(list);
     });
   },
   methods: {
+    request(sql) {
+      api.getSqlJsonAPI(sql).then((res) => {
+        let list = res.data;
+        this.list = total.rank(list);
+      });
+    },
     formatDate(date) {
-      return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+      let month = date.getMonth() + 1;
+      if (month < 10) {
+        month = "0" + month;
+      }
+      let day = date.getDate();
+      if (day < 10) {
+        day = "0" + day;
+      }
+      return `${date.getFullYear()}-${month}-${day}`;
     },
     onConfirm(date) {
+      let sql;
+
       const [start, end] = date;
       this.show = false;
       this.date = `${this.formatDate(start)} - ${this.formatDate(end)}`;
       this.startDate = this.formatDate(start);
       this.endDate = this.formatDate(end);
       // 时间change
-      let params = {
-        customer_type: "arrive_visitor",
-        start_date: this.startDate,
-        end_date: this.endDate,
-      };
-      api.getAdminSalerTopAPI(params).then((res) => {
-        let list = res.data.query_top;
-        this.list = total.rank(list);
-      });
+      switch (this.source) {
+        case "新建客户":
+          this.showData = true;
+          sql = `select saler, count(customer_name) as customer_count from fdc_form_1_1 where source ='${this.source}' and to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+          break;
+        case "来电客户":
+          this.showData = true;
+          sql = `select saler, count(customer_name) as customer_count from fdc_form_1_1 where source ='${this.source}' and to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+          break;
+        case "预约客户":
+          this.showData = true;
+          sql = `select saler, count(name) as customer_count from fdc_form_1_15 where to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+          break;
+        case "认购客户":
+          this.showData = true;
+          break;
+        case "签约客户":
+          this.showData = true;
+          break;
+
+        default:
+          this.showData = false;
+          this.$toast("暂无数据 x_x");
+          break;
+      }
+      this.request(sql);
     },
     open() {
       this.show = true;
       this.overlay = false;
     },
     change() {
+      let sql;
+
       switch (this.type) {
         case "新建客户":
           this.showData = true;
-          this.customer_type = "arrive_visitor";
+          this.source = "到访客户";
+          sql = `select saler, count(customer_name) as customer_count from fdc_form_1_1 where source ='${this.source}' and to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+          break;
+        case "来电客户":
+          this.showData = true;
+          this.source = "来电客户";
+          sql = `select saler, count(customer_name) as customer_count from fdc_form_1_1 where source ='${this.source}' and to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+          break;
+        case "预约客户":
+          this.showData = true;
+          this.source = "预约客户";
+          sql = `select saler, count(name) as customer_count from fdc_form_1_15 where to_char(created_at,'YYYY-MM-DD')>='${this.startDate}' and to_char(created_at,'YYYY-MM-DD')<='${this.endDate}' group by saler order by customer_count desc`;
+          break;
+        case "认购客户":
+          this.showData = true;
+          this.source = "认购客户";
+          break;
+        case "签约客户":
+          this.showData = true;
+          this.source = "签约客户";
           break;
 
         default:
@@ -109,15 +160,7 @@ export default {
           break;
       }
       // 类型change
-      let params = {
-        customer_type: this.customer_type,
-        start_date: this.startDate,
-        end_date: this.endDate,
-      };
-      api.getAdminSalerTopAPI(params).then((res) => {
-        let list = res.data.query_top;
-        this.list = total.rank(list);
-      });
+      this.request(sql);
     },
   },
 };
