c38111e2307ddfce2feb169928d804bc2fab2512
xanadu-zxl <xanadu.zxl@gamil.com>
2020-08-18 14:20:20 +0800

    fix：转为单引号

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import Vue from "vue";
-import Router from "vue-router";
-import routes from "./routes";
+import Vue from 'vue'
+import Router from 'vue-router'
+import routes from './routes'
 
-Vue.use(Router);
+Vue.use(Router)
 
 const router = new Router({
-  mode: "history",
-  routes
-});
+  mode: 'history',
+  routes,
+})
 
 router.beforeEach((to, from, next) => {
-  let name = to.name;
-  let tags = localStorage.getItem("user_phone");
-  let path = sessionStorage.getItem("callback");
+  let name = to.name
+  let tags = localStorage.getItem('user_phone')
+  let path = sessionStorage.getItem('callback')
   if (!path) {
     if (!tags) {
-      sessionStorage.setItem("callback", name);
-      router.push({ name: "login" });
+      sessionStorage.setItem('callback', name)
+      router.push({ name: 'login' })
     }
   }
-  next();
-});
+  next()
+})
 
-export default router;
+export default router
