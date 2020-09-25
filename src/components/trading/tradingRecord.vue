2b447e07dc76bbf69c3ee2d0eaaa947ada1ac14a
xanadu-zxl <xanadu.zxl@gamil.com>
2020-09-25 14:50:36 +0800

    feat：重构搜索框组件

--- src/components/trading/tradingRecord.vue
+++ src/components/trading/tradingRecord.vue
@@ -2,19 +2,27 @@
   <div class="detail_content">
     <header class="header">
       <!-- 筛选条件： -->
-      <select class="select" v-model="search.type">
-        <option :key="option.key" :value="option.key" class="option" v-for="option in columns">{{
-          option.title
-        }}</option>
-      </select>
-      <van-search class="search" shape="round" v-model="search.value" placeholder="请输入搜索关键词" @blur="onSearch" />
+      <div class="header-select">
+        <select class="select" v-model="search.type">
+          <option :key="option.key" :value="option.key" class="option" v-for="option in columns">{{
+            option.title
+          }}</option>
+        </select>
+        <van-search
+          class="search"
+          shape="round"
+          v-model="search.value"
+          placeholder="请输入搜索关键词"
+          @blur="onSearch"
+        />
+      </div>
+      <img class="export" @click="exportData" src="@/assets/img/table_btn_download.png" />
     </header>
-
     <Table
       ref="table"
       border
       stripe
-      height="900"
+      height="770"
       @on-row-click="rowClick"
       :columns="columns"
       :data="data"
@@ -29,14 +37,13 @@
       show-total
       class-name="page"
     />
-    <div class="export" @click="exportData" type="info">导出数据</div>
-
     <!-- 弹框 -->
     <van-popup v-model="showDetails" round closeable close-icon="close" :style="{ height: '80%', width: '90%' }">
       <div class="popup">
         <div v-for="item in showArr" :key="item.id">
           <van-field readonly :label="item.title" :value="showObj[item.identity_key]" />
         </div>
+        <router-link to="tradingRecordDetail">去修改</router-link>
       </div>
     </van-popup>
   </div>
@@ -55,15 +62,15 @@ export default {
       page: {
         pageSize: 18,
         total: 0,
-        current: 0,
+        current: 0
       },
       loading: true,
       search: {
         value: '',
-        type: 'room_number',
+        type: 'room_number'
       },
       showPhone: true,
-      tableID: '22',
+      tableID: '22'
     }
   },
   mounted() {
@@ -75,7 +82,6 @@ export default {
       this.columns = total.createdTableHeaders(res.data.fields)
     })
     const permission = localStorage.getItem('user_permission')
-
     console.log(permission.includes('销售总监'))
     this.getPageData()
     let sqlCount = `SELECT COUNT(*) FROM fdc_form_1_22;`
@@ -145,77 +151,138 @@ export default {
         this.$refs.table.data = res.data
         this.$refs.table.exportCsv({
           filename: '成交台账',
-          quoted: true,
+          quoted: true
         })
       })
-    },
-  },
+    }
+  }
 }
 </script>
 <style lang="scss">
 .detail_content {
   margin: 0px auto;
+
   .header {
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     margin-top: 20px;
     width: 90vw;
     margin: 0 auto;
 
-    .select {
-      width: 150px;
-      display: inline-block;
-      height: 28px;
-      margin: 14px 0px;
+    .header-select {
+      display: flex;
+      border-radius: 20px;
+      margin: 15px 0px;
+
+      .select {
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+        background: #f6f7f9;
+        display: inline-block;
+        width: 110px;
+        padding-left: 24px;
+        border: none;
+        outline: none;
+        height: 40px;
+      }
+      .search {
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+        height: 40px;
+        background: #f6f7f9;
+        width: 180px;
+        padding: 0px;
+      }
     }
-    .search {
-      width: 200px;
+    .export {
+      background: #1989fa;
+      height: 40px;
+      width: 40px;
+      margin: 15px;
+      border-radius: 20px;
     }
   }
+  .ivu-table-header thead tr th,
+  .ivu-table-fixed-header thead tr th {
+    padding: 4px;
+  }
+
   .ivu-table td,
   .ivu-table th {
     text-align: center;
+    border-bottom: none;
   }
   .ivu-table th {
-    color: #000;
+    color: #fff;
+    background: #6788e7;
     font-weight: 600;
   }
   .ivu-table-cell {
-    height: 48px;
-    line-height: 48px;
+    height: 46px;
+    line-height: 46px;
   }
-
   .ivu-table-row {
-    height: 48px;
+    height: 46px;
   }
-
-  .ivu-table td,
-  .ivu-table th {
-    height: 48px;
+  .table {
+    position: relative;
+    .page {
+      position: absolute;
+      bottom: -150px;
+      width: 100%;
+      right: 50%;
+      transform: translateX(50%);
+    }
+    .ivu-page-total {
+      font-size: 14px;
+      position: absolute;
+      bottom: 80px;
+      right: 50%;
+      transform: translateX(50%);
+    }
   }
-
-  .page {
-    margin-top: 20px;
-  }
-
   .popup {
     margin: 30px auto;
     width: 77%;
+    position: relative;
+    .van-cell {
+      border-bottom: 1px solid #ebedf0;
+    }
   }
-
   .van-field__label {
     width: 7rem;
   }
-  .export {
-    background: #1989fa;
-    width: 100px;
-    height: 37px;
-    line-height: 37px;
+  .ivu-page-item:hover {
+    border: 1px solid #dcdee2;
+    a {
+      color: black;
+    }
+  }
+  .ivu-page-custom-text {
+    width: 88px;
+    height: 40px;
+    background: #ffffff;
+    border: 1px solid #dcdee2;
+    border-radius: 4px;
+    padding: 5px 2px;
+  }
+  .ivu-page-item-active {
+    background: #6788e7;
+  }
+  .ivu-page-item-active a,
+  .ivu-page-item-active:hover a {
     color: #fff;
-    font-size: 15px;
+  }
+  .ivu-page-next:hover a,
+  .ivu-page-prev:hover a {
+    color: #6788e7;
+  }
+  .popup-header {
+    line-height: 52px;
+    font-size: 16px;
     font-weight: 600;
-    margin: 10px auto;
-    border-radius: 7px;
+    height: 52px;
+    border-bottom: 1px solid #ebedf0;
   }
 }
 </style>
